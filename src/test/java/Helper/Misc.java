c90b5a0d6f7c36a86d4a3f163cccc9b6ee92e853
Lolieana Botros <lolieanabotros@lolieanas-air.mynetworksettings.com>
2022-05-08 20:38:24 -0400

    Framework Setup

---
+++ src/test/java/Helper/Misc.java
@@ -0,0 +1,15 @@
+package Helper;
+
+public class Misc {
+
+
+    public static void pause(int seconds) {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
